3f87758397707fd1aa3caba92864167401a66de6
TemoshoS <temoshomaduane@gmail.vom>
2023-08-15 11:38:59 +0200

    first commit

--- pages/BirthdayCard.js
+++ pages/BirthdayCard.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, TextInput, View, Button, TouchableOpacity, ImageBackground } from 'react-native'
+import { StyleSheet, Text, TextInput, View, Button, TouchableOpacity, Image } from 'react-native'
 import React, { useState } from 'react'
 
 
@@ -10,60 +10,83 @@ export default function BirthdayCard() {
     const handleInputChange = (text) => {
         setMessage(text);
     }
-    const handleSendWishes =()=>{
+    const handleSendWishes = () => {
         setDisplayMesage(message);
     }
-    
+
     return (
-        <ImageBackground source={require('../assets/back.jpg')} style={styles.background} resizeMode='cover'>
-            
-            <View style={styles.container} >
-
-                <Text style={styles.header}>Happy Birthday!</Text>
 
 
-                <TextInput
-                    style={styles.input}
-                    placeholder="Enter message"
-                    onChangeText={handleInputChange}
-                    multiline={true}
-                    
-                />
-               
-
-                <TouchableOpacity
-                    style={styles.button}
-                    onPress={handleSendWishes}
-                    
-                >
-                    <Text style={styles.buttonText}>Send Wishes</Text>
-                </TouchableOpacity>
-                
-                {displayMessage ? (
-                    <View style={styles.msgContainer}>
-                    <Text style={styles.msg} multiline={true} >{displayMessage}</Text>
-                    </View>
-                ):null}
+        <View style={styles.container} >
+            <View style={styles.happy}>
+            <Text style={[styles.header, { color: '#BA55D3' }]}>H </Text>
+            <Text style={[styles.header, { color: '#90EE90' }]}>A </Text>
+            <Text style={[styles.header, { color: '#FFA07A' }]}>P </Text>
+            <Text style={[styles.header, { color: '#87CEFA' }]}>P </Text>
+            <Text style={[styles.header, { color: '#FF6347' }]}>Y </Text>
             </View>
-        </ImageBackground>
+            <View style={styles.birthday}>
+            <Text style={[styles.header, { color: '#191970' }]}> B</Text>
+            <Text style={[styles.header, { color: '#708090' }]}> I</Text>
+            <Text style={[styles.header, { color: '#D8BFD8' }]}> R</Text>
+            <Text style={[styles.header, { color: '#FFFF00' }]}> T</Text>
+            <Text style={[styles.header, { color: '#F5DEB3' }]}> H</Text>
+            <Text style={[styles.header, { color: '#800000' }]}> D</Text>
+            <Text style={[styles.header, { color: '#7FFFD4' }]}> A</Text>
+            <Text style={[styles.header, { color: 'green' }]}> Y</Text>
+            <Text style={[styles.header, { color: '#FF7F50' }]}> !</Text>
+            </View>
+
+            <View style={styles.imageContainer}>
+                <Image source={require('../assets/cake.jpg')} style={styles.image}/>
+
+            </View>
+
+            <TextInput
+                style={styles.input}
+                placeholder="Enter message"
+                onChangeText={handleInputChange}
+                multiline={true}
+
+            />
+
+
+            <TouchableOpacity
+                style={styles.button}
+                onPress={handleSendWishes}
+
+            >
+                <Text style={styles.buttonText}>Send Wishes</Text>
+            </TouchableOpacity>
+
+            {displayMessage ? (
+                <View style={styles.msgContainer}>
+                    <Text style={styles.msg} multiline={true} >{displayMessage}</Text>
+                </View>
+            ) : null}
+        </View>
+
     )
 }
 
 const styles = StyleSheet.create({
-    background: {
-        flex: 1,
-       
-       
-       
-    },
+
     container: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
         padding: 20,
-        width: '100%'
+        backgroundColor: '#F0F8FF'
 
 
+
+    },
+    happy:{
+        flexDirection: 'row'
+    },
+    birthday:{
+        flexDirection: 'row',
+        
     },
     header: {
         fontSize: 24,
@@ -77,19 +100,33 @@ const styles = StyleSheet.create({
         textShadowRadius: 5,
 
     },
+    imageContainer:{
+        alignItems: 'center',
+        marginBottom: 20,
+        transform: [{rotate: '-30deg'}]
+    },
+    image:{
+        height: 120,
+        marginBottom: 20,
+        width: 120,
+        borderRadius: 60
+
+    },
     input: {
         width: '100%',
         borderRadius: 20,
-        height: 100,
+        height: 50,
         borderWidth: 1,
-        borderColor: 'gray',
+        borderColor: '#A9A9A9',
         textAlign: 'center',
         marginBottom: 20,
+        color: '#A9A9A9',
+
 
 
     },
-    msgContainer:{
-        width: '100%',
+    msgContainer: {
+        width: 100,
         height: 'auto',
         justifyContent: 'center',
         alignItems: 'center',
@@ -100,7 +137,7 @@ const styles = StyleSheet.create({
     msg: {
         fontSize: 25,
         fontFamily: 'Handjet-Light',
-        textAlign: 'left',
+        textAlign: 'right',
 
 
     },
